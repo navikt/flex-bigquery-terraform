6f2e8c7e1fd9dc98654a555cc48b8c43e979a8d3
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-11-25 09:53:33 +0100

    Hent data fra spinnsyn_annullering

--- prod/spinnsyn-backend.tf
+++ prod/spinnsyn-backend.tf
@@ -129,3 +129,73 @@ EXTERNAL_QUERY('${var.gcp_project["project"]}.${var.gcp_project["region"]}.spinn
 EOF
 
 }
+
+
+module "spinnsyn_annullering" {
+  source = "../modules/google-bigquery-table"
+
+  location   = var.gcp_project["region"]
+  dataset_id = module.flex_dataset.dataset_id
+  table_id   = "spinnsyn_annullering"
+  table_schema = jsonencode(
+    [
+      {
+        mode = "NULLABLE"
+        name = "id"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "fnr"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "annullering"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "opprettet"
+        type = "TIMESTAMP"
+      },
+    ]
+  )
+
+  view_id = "spinnsyn_annullering_view"
+  view_schema = jsonencode(
+    [
+      {
+        mode        = "NULLABLE"
+        name        = "id"
+        type        = "STRING"
+        description = "Unik ID for annulleringen."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "opprettet"
+        type        = "TIMESTAMP"
+        description = "Når annulleringen ble opprettet."
+      },
+    ]
+  )
+
+  view_query = <<EOF
+SELECT id, opprettet
+FROM `${var.gcp_project["project"]}.${module.flex_dataset.dataset_id}.${module.spinnsyn_annullering.bigquery_table_id}`
+EOF
+
+  data_transfer_display_name      = "spinnsyn_annullering_query"
+  data_transfer_schedule          = "every day 02:00"
+  data_transfer_service_account   = "federated-query@${var.gcp_project["project"]}.iam.gserviceaccount.com"
+  data_transfer_start_time        = "2022-11-26T00:00:00Z"
+  data_transfer_destination_table = module.spinnsyn_annullering.bigquery_table_id
+  data_transfer_mode              = "WRITE_TRUNCATE"
+
+  data_transfer_query = <<EOF
+SELECT * FROM
+EXTERNAL_QUERY('${var.gcp_project["project"]}.${var.gcp_project["region"]}.spinnsyn-backend',
+'SELECT id, fnr, annullering, opprettet FROM annullering');
+EOF
+
+}
