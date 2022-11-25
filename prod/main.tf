52ec1336fd9c5811789699ade1bf79e87e73de72
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-11-25 09:53:03 +0100

    Hent data fra spinnsyn_utbetaling

--- prod/main.tf
+++ prod/main.tf
@@ -15,20 +15,12 @@ provider "google" {
   region  = var.gcp_project["region"]
 }
 
-data "google_secret_manager_secret_version" "spinnsyn_bigquery_secret" {
-  secret = var.spinnsyn_bigquery_secret
-}
-
 data "google_project" "project" {}
 
 locals {
   google_project_iam_member = {
     email = "service-${data.google_project.project.number}@gcp-sa-bigquerydatatransfer.iam.gserviceaccount.com"
   }
-
-  spinnsyn_db = jsondecode(
-    data.google_secret_manager_secret_version.spinnsyn_bigquery_secret.secret_data
-  )
 }
 
 module "google_storage_bucket" {
@@ -43,15 +35,4 @@ module "flex_dataset" {
   dataset_id         = "flex_dataset"
   location           = var.gcp_project["region"]
   dataset_iam_member = local.google_project_iam_member.email
-}
-
-module "spinnsyn_bigquery_connection" {
-  source = "../modules/google-bigquery-connection"
-
-  connection_id = "spinnsyn-backend"
-  location      = var.gcp_project["region"]
-  instance_id   = "${var.gcp_project["project"]}:${var.gcp_project["region"]}:spinnsyn-backend"
-  database      = "spinnsyn-db"
-  username      = local.spinnsyn_db.username
-  password      = local.spinnsyn_db.password
 }
