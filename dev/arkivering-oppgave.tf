e5fa7465874efe20bcaf95318f2f2e16f083dd6c
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-12-07 15:09:53 +0100

    Legg til tabell arkivering-oppgave oppgavestyring

--- dev/arkivering-oppgave.tf
+++ dev/arkivering-oppgave.tf
@@ -69,3 +69,64 @@ EXTERNAL_QUERY('${var.gcp_project["project"]}.${var.gcp_project["region"]}.sykep
 EOF
 
 }
+
+module "sykepengesoknad_arkivering_oppgave_oppgavestyring" {
+  source = "../modules/google-bigquery-table"
+
+  location   = var.gcp_project["region"]
+  dataset_id = google_bigquery_dataset.flex_dataset.dataset_id
+  table_id   = "sykepengesoknad_arkivering_oppgave_oppgavestyring"
+  table_schema = jsonencode(
+    [
+      {
+        mode = "NULLABLE"
+        name = "id"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "sykepengesoknad_id"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "status"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "opprettet"
+        type = "TIMESTAMP"
+      },
+      {
+        mode = "NULLABLE"
+        name = "modifisert"
+        type = "TIMESTAMP"
+      },
+      {
+        mode = "NULLABLE"
+        name = "timeout"
+        type = "TIMESTAMP"
+      },
+      {
+        mode = "NULLABLE"
+        name = "avstemte"
+        type = "BOOLEAN"
+      },
+    ]
+  )
+
+  data_transfer_display_name      = "sykepengesoknad_arkivering_oppgave_oppgavestyring_query"
+  data_transfer_schedule          = "every day 02:45"
+  data_transfer_service_account   = "federated-query@${var.gcp_project["project"]}.iam.gserviceaccount.com"
+  data_transfer_start_time        = "2022-12-07T00:00:00Z"
+  data_transfer_destination_table = module.sykepengesoknad_arkivering_oppgave_oppgavestyring.bigquery_table_id
+  data_transfer_mode              = "WRITE_TRUNCATE"
+
+  data_transfer_query = <<EOF
+SELECT * FROM
+EXTERNAL_QUERY('${var.gcp_project["project"]}.${var.gcp_project["region"]}.sykepengesoknad-arkivering-oppgave',
+'SELECT id, sykepengesoknad_id, status, opprettet, modifisert, timeout, avstemt FROM oppgavestyring');
+EOF
+
+}
