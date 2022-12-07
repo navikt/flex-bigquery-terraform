f2c94a292a1b58f7614e87cb0568b67db54cf292
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2022-12-07 17:33:22 +0100

    Hent alle tabeller fra spinnsyn i dev

--- dev/spinnsyn-backend.tf
+++ dev/spinnsyn-backend.tf
@@ -22,9 +22,10 @@ module "spinnsyn_bigquery_connection" {
 module "spinnsyn_utbetaling" {
   source = "../modules/google-bigquery-table"
 
-  location   = var.gcp_project["region"]
-  dataset_id = google_bigquery_dataset.flex_dataset.dataset_id
-  table_id   = "spinnsyn_utbetaling"
+  deletion_protection = false
+  location            = var.gcp_project["region"]
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  table_id            = "spinnsyn_utbetaling"
   table_schema = jsonencode(
     [
       {
@@ -83,7 +84,7 @@ module "spinnsyn_utbetaling" {
   data_transfer_display_name      = "spinnsyn_utbetaling_query"
   data_transfer_schedule          = "every day 02:00"
   data_transfer_service_account   = "federated-query@${var.gcp_project["project"]}.iam.gserviceaccount.com"
-  data_transfer_start_time        = "2022-11-10T00:00:00Z"
+  data_transfer_start_time        = "2022-12-08T00:00:00Z"
   data_transfer_destination_table = module.spinnsyn_utbetaling.bigquery_table_id
   data_transfer_mode              = "WRITE_TRUNCATE"
 
@@ -98,8 +99,9 @@ EOF
 module "spinnsyn_utbetaling_view" {
   source = "../modules/google-bigquery-view"
 
-  dataset_id = google_bigquery_dataset.flex_dataset.dataset_id
-  view_id    = "spinnsyn_utbetaling_view"
+  deletion_protection = false
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  view_id             = "spinnsyn_utbetaling_view"
   view_schema = jsonencode(
     [
       {
@@ -128,19 +130,20 @@ module "spinnsyn_utbetaling_view" {
       },
     ]
   )
-
   view_query = <<EOF
 SELECT utbetaling_id, utbetaling_type, opprettet, antall_vedtak
 FROM `${var.gcp_project["project"]}.${google_bigquery_dataset.flex_dataset.dataset_id}.${module.spinnsyn_utbetaling.bigquery_table_id}`
 EOF
+
 }
 
 module "spinnsyn_annullering" {
   source = "../modules/google-bigquery-table"
 
-  location   = var.gcp_project["region"]
-  dataset_id = google_bigquery_dataset.flex_dataset.dataset_id
-  table_id   = "spinnsyn_annullering"
+  deletion_protection = false
+  location            = var.gcp_project["region"]
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  table_id            = "spinnsyn_annullering"
   table_schema = jsonencode(
     [
       {
@@ -169,7 +172,7 @@ module "spinnsyn_annullering" {
   data_transfer_display_name      = "spinnsyn_annullering_query"
   data_transfer_schedule          = "every day 02:00"
   data_transfer_service_account   = "federated-query@${var.gcp_project["project"]}.iam.gserviceaccount.com"
-  data_transfer_start_time        = "2022-11-10T00:00:00Z"
+  data_transfer_start_time        = "2022-12-08T00:00:00Z"
   data_transfer_destination_table = module.spinnsyn_annullering.bigquery_table_id
   data_transfer_mode              = "WRITE_TRUNCATE"
 
@@ -180,12 +183,12 @@ EXTERNAL_QUERY('${var.gcp_project["project"]}.${var.gcp_project["region"]}.spinn
 EOF
 
 }
-
 module "spinnsyn_annullering_view" {
   source = "../modules/google-bigquery-view"
 
-  dataset_id = google_bigquery_dataset.flex_dataset.dataset_id
-  view_id    = "spinnsyn_annullering_view"
+  deletion_protection = false
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  view_id             = "spinnsyn_annullering_view"
   view_schema = jsonencode(
     [
       {
@@ -206,6 +209,286 @@ module "spinnsyn_annullering_view" {
   view_query = <<EOF
 SELECT id, opprettet
 FROM `${var.gcp_project["project"]}.${google_bigquery_dataset.flex_dataset.dataset_id}.${module.spinnsyn_annullering.bigquery_table_id}`
+EOF
+
+}
+
+module "spinnsyn_done_vedtak" {
+  source = "../modules/google-bigquery-table"
+
+  deletion_protection = false
+  location            = var.gcp_project["region"]
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  table_id            = "spinnsyn_done_vedtak"
+  table_schema = jsonencode(
+    [
+      {
+        mode = "NULLABLE"
+        name = "id"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "fnr"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "type"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "done_sendt"
+        type = "TIMESTAMP"
+      },
+    ]
+  )
+
+  data_transfer_display_name      = "spinnsyn_done_vedtak_query"
+  data_transfer_schedule          = "every day 02:00"
+  data_transfer_service_account   = "federated-query@${var.gcp_project["project"]}.iam.gserviceaccount.com"
+  data_transfer_start_time        = "2022-12-08T00:00:00Z"
+  data_transfer_destination_table = module.spinnsyn_done_vedtak.bigquery_table_id
+  data_transfer_mode              = "WRITE_TRUNCATE"
+
+  data_transfer_query = <<EOF
+SELECT * FROM
+EXTERNAL_QUERY('${var.gcp_project["project"]}.${var.gcp_project["region"]}.spinnsyn-backend',
+'SELECT id, fnr, type, done_sendt FROM done_vedtak');
+EOF
+
+}
+module "spinnsyn_done_vedtak_view" {
+  source = "../modules/google-bigquery-view"
+
+  deletion_protection = false
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  view_id             = "spinnsyn_done_vedtak_view"
+  view_schema = jsonencode(
+    [
+      {
+        mode        = "NULLABLE"
+        name        = "id"
+        type        = "STRING"
+        description = "Unik ID for done-meldingen."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "type"
+        type        = "STRING"
+        description = "Om done-meldingen gjelder VEDTAK eller UTBETALING."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "done_sendt"
+        type        = "TIMESTAMP"
+        description = "Når done-melding ble sendt."
+      },
+    ]
+  )
+
+  view_query = <<EOF
+SELECT id, type, done_sendt
+FROM `${var.gcp_project["project"]}.${google_bigquery_dataset.flex_dataset.dataset_id}.${module.spinnsyn_done_vedtak.bigquery_table_id}`
+EOF
+
+}
+
+module "spinnsyn_organisasjon" {
+  source = "../modules/google-bigquery-table"
+
+  deletion_protection = false
+  location            = var.gcp_project["region"]
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  table_id            = "spinnsyn_organisasjon"
+  table_schema = jsonencode(
+    [
+      {
+        mode = "NULLABLE"
+        name = "id"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "orgnummer"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "navn"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "opprettet"
+        type = "TIMESTAMP"
+      },
+      {
+        mode = "NULLABLE"
+        name = "oppdatert"
+        type = "TIMESTAMP"
+      },
+      {
+        mode = "NULLABLE"
+        name = "oppdatert_av"
+        type = "STRING"
+      },
+    ]
+  )
+
+  data_transfer_display_name      = "spinnsyn_organisasjon_query"
+  data_transfer_schedule          = "every day 02:00"
+  data_transfer_service_account   = "federated-query@${var.gcp_project["project"]}.iam.gserviceaccount.com"
+  data_transfer_start_time        = "2022-12-08T00:00:00Z"
+  data_transfer_destination_table = module.spinnsyn_organisasjon.bigquery_table_id
+  data_transfer_mode              = "WRITE_TRUNCATE"
+
+  data_transfer_query = <<EOF
+SELECT * FROM
+EXTERNAL_QUERY('${var.gcp_project["project"]}.${var.gcp_project["region"]}.spinnsyn-backend',
+'SELECT id, orgnummer, navn, opprettet, oppdatert, oppdatert_av FROM organisasjon');
+EOF
+
+}
+module "spinnsyn_organisasjon_view" {
+  source = "../modules/google-bigquery-view"
+
+  deletion_protection = false
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  view_id             = "spinnsyn_organisasjon_view"
+  view_schema = jsonencode(
+    [
+      {
+        mode        = "NULLABLE"
+        name        = "id"
+        type        = "STRING"
+        description = "Unik ID for organisasjonen."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "orgnummer"
+        type        = "STRING"
+        description = "Organisasjonens orgnummer."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "navn"
+        type        = "STRING"
+        description = "Organisasjonens navn."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "opprettet"
+        type        = "TIMESTAMP"
+        description = "Når organisasjonen ble opprettet."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "oppdatert"
+        type        = "TIMESTAMP"
+        description = "Når organisasjonen ble sist ble oppdatert."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "oppdatert_av"
+        type        = "STRING"
+        description = "Unik ID på oppdateringskilden."
+      },
+    ]
+  )
+
+  view_query = <<EOF
+SELECT id, orgnummer, navn, opprettet, oppdatert, oppdatert_av
+FROM `${var.gcp_project["project"]}.${google_bigquery_dataset.flex_dataset.dataset_id}.${module.spinnsyn_organisasjon.bigquery_table_id}`
+EOF
+
+}
+
+module "spinnsyn_vedtak" {
+  source = "../modules/google-bigquery-table"
+
+  deletion_protection = false
+  location            = var.gcp_project["region"]
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  table_id            = "spinnsyn_vedtak"
+  table_schema = jsonencode(
+    [
+      {
+        mode = "NULLABLE"
+        name = "id"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "fnr"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "vedtak"
+        type = "STRING"
+      },
+      {
+        mode = "NULLABLE"
+        name = "opprettet"
+        type = "TIMESTAMP"
+      },
+      {
+        mode = "NULLABLE"
+        name = "utbetaling_id"
+        type = "STRING"
+      },
+    ]
+
+  )
+
+  data_transfer_display_name      = "spinnsyn_vedtak_query"
+  data_transfer_schedule          = "every day 02:00"
+  data_transfer_service_account   = "federated-query@${var.gcp_project["project"]}.iam.gserviceaccount.com"
+  data_transfer_start_time        = "2022-12-08T00:00:00Z"
+  data_transfer_destination_table = module.spinnsyn_vedtak.bigquery_table_id
+  data_transfer_mode              = "WRITE_TRUNCATE"
+
+  data_transfer_query = <<EOF
+SELECT * FROM
+EXTERNAL_QUERY('${var.gcp_project["project"]}.${var.gcp_project["region"]}.spinnsyn-backend',
+'SELECT id, fnr, vedtak, opprettet, utbetaling_id FROM vedtak_v2');
+EOF
+
+}
+module "spinnsyn_vedtak_view" {
+  source = "../modules/google-bigquery-view"
+
+  deletion_protection = false
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  view_id             = "spinnsyn_vedtak_view"
+  view_schema = jsonencode(
+    [
+      {
+        mode        = "NULLABLE"
+        name        = "id"
+        type        = "STRING"
+        description = "Unik ID for vedtaket."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "opprettet"
+        type        = "TIMESTAMP"
+        description = "Når vedtaket ble opprettet."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "utbetaling_id"
+        type        = "STRING"
+        description = "Unik ID på utbetaling tilhørende vedtaket."
+      },
+    ]
+  )
+
+  view_query = <<EOF
+SELECT id, opprettet, utbetaling_id
+FROM `${var.gcp_project["project"]}.${google_bigquery_dataset.flex_dataset.dataset_id}.${module.spinnsyn_vedtak.bigquery_table_id}`
 EOF
 
 }
