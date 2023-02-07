ec24ed7d94a0112baf7060828439286e863262e2
Håvard Stigen Andersen <havard.stigen.andersen@nav.no>
2023-02-07 08:57:52 +0100

    Deletion protection false

--- dev/sykepengesoknad-backend.tf
+++ dev/sykepengesoknad-backend.tf
@@ -1016,7 +1016,7 @@ EOF
 
 module "sykepengesoknad_hovedsporsmal_pivot_view" {
   source = "../modules/google-bigquery-view"
-
+  deletion_protection = "false"
   dataset_id = google_bigquery_dataset.flex_dataset.dataset_id
   view_id    = "sykepengesoknad_hovedsporsmal_pivot_view"
   view_schema = jsonencode(
