f1bddf634f1f9abaad6732ffe1dca14a409a2e5b
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-02-18 07:25:58 +0100

    Opprett VPC Private Connectivity

--- dev/datastream.tf
+++ dev/datastream.tf
@@ -16,3 +16,34 @@ locals {
     data.google_secret_manager_secret_version.sykepengesoknad_datastream_secret.secret_data
   )
 }
+
+// TODO:  Sjekk om det er mulig å definere "flex-datastream-vpc-ip-range" tilhørende VPC her.
+
+// Private connectivity lets you create a peered configuration between your VPC and Datastream’s private network.
+// A single configuration can be used by all streams and connection profiles within a single region.
+resource "google_datastream_private_connection" "flex_datastream_private_connection" {
+  location              = var.gcp_project["region"]
+  display_name          = "flex-datastream-private-connection"
+  private_connection_id = "flex-datastream-private-connection"
+
+  vpc_peering_config {
+    vpc    = google_compute_network.flex_datastream_private_vpc.id
+    subnet = "10.1.0.0/29"
+  }
+}
+
+// VPX Firewall rules control incoming or outgoing traffic to an instance. By default, incoming traffic from outside
+// your network is blocked. Since we are using a Cloud SQL reverse proxy, we need to then create an ingress firewall
+// rule that allows traffic on the source database port.
+resource "google_compute_firewall" "allow_datastream_to_cloud_sql" {
+  project = var.gcp_project["project"]
+  name    = "allow-datastream-to-cloud-sql"
+  network = google_compute_network.flex_datastream_private_vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5432"]
+  }
+
+  source_ranges = [google_datastream_private_connection.flex_datastream_private_connection.vpc_peering_config.0.subnet]
+}
