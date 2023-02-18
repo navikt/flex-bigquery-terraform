7c9f4d59f7c2c595f8ee38a7e070f97588b118b5
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-02-18 07:25:14 +0100

    Konfigurer secrets i dev

--- dev/datastream.tf
+++ dev/datastream.tf
@@ -1,4 +1,18 @@
 resource "google_compute_network" "flex_datastream_private_vpc" {
-  name = "flex-datastream-vpc"
-  project =  var.gcp_project["project"]
+  name    = "flex-datastream-vpc"
+  project = var.gcp_project["project"]
+}
+
+data "google_sql_database_instance" "sykepengesoknad_db" {
+  name = "sykepengesoknad"
+}
+
+data "google_secret_manager_secret_version" "sykepengesoknad_datastream_secret" {
+  secret = var.sykepengesoknad_datastream_secret
+}
+
+locals {
+  sykepengesoknad_datastream_credentials = jsondecode(
+    data.google_secret_manager_secret_version.sykepengesoknad_datastream_secret.secret_data
+  )
 }
