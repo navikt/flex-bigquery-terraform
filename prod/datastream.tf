9acda1be120933657efea886cb7b5f59d937c5e8
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-03-02 11:31:02 +0100

    Importer flex_datastream_vpc_ip_range i prod

--- prod/datastream.tf
+++ prod/datastream.tf
@@ -17,6 +17,18 @@ locals {
   )
 }
 
+// The IP-range in the VPC used for the Datastream VPC peering.
+resource "google_compute_global_address" "flex_datastream_vpc_ip_range" {
+  name          = "flex-datastream-vpc-ip-range"
+  project       = var.gcp_project["project"]
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  network       = google_compute_network.flex_datastream_private_vpc.id
+  address       = "10.18.0.0"
+  prefix_length = 20
+
+}
+
 // Private connectivity lets you create a peered configuration between your VPC and Datastream’s private network.
 // A single configuration can be used by all streams and connection profiles within a single region.
 resource "google_datastream_private_connection" "flex_datastream_private_connection" {
