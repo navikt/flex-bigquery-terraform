48ae622696d443e3916febfcc4bc086bbceb4a48
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-03-06 10:12:50 +0100

    Spellcheck

--- prod/datastream-arkivering-oppgave.tf
+++ prod/datastream-arkivering-oppgave.tf
@@ -30,7 +30,6 @@ locals {
   )
 }
 
-// Datastrean connection profile for PostgreSQL source. Used to create the Datastream.
 resource "google_datastream_connection_profile" "arkivering_oppgave_postgresql_connection_profile" {
   location              = var.gcp_project["region"]
   display_name          = "arkivering-oppgave-postgresql-connection-profile"
