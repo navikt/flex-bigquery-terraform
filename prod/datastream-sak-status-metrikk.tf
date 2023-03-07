60a66d7af872a30160078415c641b2bbbf03c4ae
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-03-07 14:04:01 +0100

    Opprett dataset sak_status_metrikk_datastream

---
+++ prod/datastream-sak-status-metrikk.tf
@@ -0,0 +1,21 @@
+resource "google_bigquery_dataset" "sak_status_metrikk_datastream" {
+  dataset_id = "sak_status_metrikk_datastream"
+  location   = var.gcp_project["region"]
+  project    = var.gcp_project["project"]
+  labels     = {}
+
+  access {
+    role          = "OWNER"
+    special_group = "projectOwners"
+  }
+  access {
+    role          = "READER"
+    special_group = "projectReaders"
+  }
+  access {
+    role          = "WRITER"
+    special_group = "projectWriters"
+  }
+
+  timeouts {}
+}
