55034da7a9c97b3f52684ff32453e09850590b2f
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-03-07 14:32:37 +0100

    Opprett connection profile for sak_status_metrikk

--- prod/datastream-sak-status-metrikk.tf
+++ prod/datastream-sak-status-metrikk.tf
@@ -19,3 +19,21 @@ resource "google_bigquery_dataset" "sak_status_metrikk_datastream" {
 
   timeouts {}
 }
+
+resource "google_datastream_connection_profile" "sak_status_metrikk_postgresql_connection_profile" {
+  location              = var.gcp_project["region"]
+  display_name          = "sak-status-metrikk-postgresql-connection-profile"
+  connection_profile_id = "sak-status-metrikk-postgresql-connection-profile"
+
+  postgresql_profile {
+    hostname = google_compute_instance.flex_datastream_cloud_sql_proxy_vm.network_interface[0].network_ip
+    port     = var.sak_status_metrikk_cloud_sql_port
+    username = local.sak_status_metrikk_datastream_credentials["username"]
+    password = local.sak_status_metrikk_datastream_credentials["password"]
+    database = "sykepengesoknad-sak-status-metrikk-db"
+  }
+
+  private_connectivity {
+    private_connection = google_datastream_private_connection.flex_datastream_private_connection.id
+  }
+}
