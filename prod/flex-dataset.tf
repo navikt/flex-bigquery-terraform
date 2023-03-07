ffb478b39a7c9157df3d5a4f83c5304cdff5cff7
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-03-07 20:27:13 +0100

    Opprett BigQuery External Connections

--- prod/flex-dataset.tf
+++ prod/flex-dataset.tf
@@ -177,3 +177,47 @@ resource "google_bigquery_table_iam_binding" "sykepengesoknad_sak_status_metrikk
     "serviceAccount:nada-metabase@nada-prod-6977.iam.gserviceaccount.com",
   ]
 }
+
+module "sykepengesoknad_bigquery_connection" {
+  source = "../modules/google-bigquery-connection"
+
+  connection_id = "sykepengesoknad-backend"
+  location      = var.gcp_project["region"]
+  instance_id   = "${var.gcp_project["project"]}:${var.gcp_project["region"]}:sykepengesoknad"
+  database      = "sykepengesoknad"
+  username      = local.sykepengesoknad_db.username
+  password      = local.sykepengesoknad_db.password
+}
+
+module "spinnsyn_arkivering_oppgave_connection" {
+  source = "../modules/google-bigquery-connection"
+
+  connection_id = "sykepengesoknad-arkivering-oppgave"
+  location      = var.gcp_project["region"]
+  instance_id   = "${var.gcp_project["project"]}:${var.gcp_project["region"]}:sykepengesoknad-arkivering-oppgave"
+  database      = "sykepengesoknad-arkivering-oppgave-db"
+  username      = local.arkivering_oppgave_db.username
+  password      = local.arkivering_oppgave_db.password
+}
+
+module "spinnsyn_bigquery_connection" {
+  source = "../modules/google-bigquery-connection"
+
+  connection_id = "spinnsyn-backend"
+  location      = var.gcp_project["region"]
+  instance_id   = "${var.gcp_project["project"]}:${var.gcp_project["region"]}:spinnsyn-backend"
+  database      = "spinnsyn-db"
+  username      = local.spinnsyn_db.username
+  password      = local.spinnsyn_db.password
+}
+
+module "sykepengesoknad_sak-status-metrikk-bigquery_connection" {
+  source = "../modules/google-bigquery-connection"
+
+  connection_id = "sykepengesoknad-sak-status-metrikk"
+  location      = var.gcp_project["region"]
+  instance_id   = "${var.gcp_project["project"]}:${var.gcp_project["region"]}:sykepengesoknad-sak-status-metrikk"
+  database      = "sykepengesoknad-sak-status-metrikk-db"
+  username      = local.sak_status_metrikk_db.username
+  password      = local.sak_status_metrikk_db.password
+}
