19850328386ddc5c814b301176df2ccf05852777
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-03-25 10:33:57 +0100

    Klargjør for Datastream fra spinnsyn-backend

---
+++ dev/datastream-spinnsyn.tf
@@ -0,0 +1,38 @@
+resource "google_bigquery_dataset" "spinnsyn_datastream" {
+  dataset_id = "spinnsyn_datastream"
+  location   = var.gcp_project["region"]
+  project    = var.gcp_project["project"]
+  labels     = {}
+
+  access {
+    role          = "OWNER"
+    special_group = "projectOwners"
+  }
+  access {
+    role          = "READER"
+    special_group = "projectReaders"
+  }
+  access {
+    role          = "WRITER"
+    special_group = "projectWriters"
+  }
+  timeouts {}
+}
+
+resource "google_datastream_connection_profile" "spinnsyn_postgresql_connection_profile" {
+  location              = var.gcp_project["region"]
+  display_name          = "spinnsyn-postgresql-connection-profile"
+  connection_profile_id = "spinnsyn-postgresql-connection-profile"
+
+  postgresql_profile {
+    hostname = google_compute_instance.flex_datastream_cloud_sql_proxy_vm.network_interface[0].network_ip
+    port     = var.spinnsyn_cloud_sql_port
+    username = local.spinnsyn_datastream_credentials["username"]
+    password = local.spinnsyn_datastream_credentials["password"]
+    database = "spinnsyn-db"
+  }
+
+  private_connectivity {
+    private_connection = google_datastream_private_connection.flex_datastream_private_connection.id
+  }
+}
--- dev/datastream-vpc.tf
+++ dev/datastream-vpc.tf
@@ -28,4 +28,89 @@ resource "google_datastream_private_connection" "flex_datastream_private_connect
   }
 }
 
+// VPC Firewall rules control incoming or outgoing traffic to an instance. By default, incoming traffic from outside
+// your network is blocked. Since we are using a Cloud SQL reverse proxy, we need to then create an ingress firewall
+// rule that allows traffic on the source database port.
+resource "google_compute_firewall" "allow_datastream_to_cloud_sql" {
+  project = var.gcp_project["project"]
+  name    = "allow-datastream-to-cloud-sql"
+  network = google_compute_network.flex_datastream_private_vpc.name
 
+  allow {
+    protocol = "tcp"
+    ports = [
+      var.spinnsyn_cloud_sql_port,
+    ]
+  }
+
+  source_ranges = [google_datastream_private_connection.flex_datastream_private_connection.vpc_peering_config.0.subnet]
+}
+
+data "google_sql_database_instance" "spinnsyn_db" {
+  name = "spinnsyn-backend"
+}
+
+locals {
+  proxy_instances = [
+    "${data.google_sql_database_instance.spinnsyn_db.connection_name}=tcp:0.0.0.0:${var.spinnsyn_cloud_sql_port}",
+  ]
+}
+
+// This module handles the generation of metadata used to create an instance used to host containers on GCE.
+// The module itself does not launch an instance or managed instance group.
+module "cloud_sql_auth_proxy_container_datastream" {
+  source         = "terraform-google-modules/container-vm/google"
+  version        = "3.1.0"
+  cos_image_name = "cos-stable-101-17162-127-8"
+  container = {
+    image   = "eu.gcr.io/cloudsql-docker/gce-proxy:1.33.2"
+    command = ["/cloud_sql_proxy"]
+    args = [
+      "-instances=${join(",", local.proxy_instances)}",
+      "-ip_address_types=PRIVATE"
+    ]
+  }
+  restart_policy = "Always"
+}
+
+// Create a VM used to host the Cloud SQL reverse proxy.
+resource "google_compute_instance" "flex_datastream_cloud_sql_proxy_vm" {
+  name         = "flex-datastream-cloud-sql-proxy-vm"
+  machine_type = "e2-micro"
+  project      = var.gcp_project["project"]
+  zone         = var.gcp_project["zone"]
+
+  boot_disk {
+    initialize_params {
+      image = module.cloud_sql_auth_proxy_container_datastream.source_image
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.flex_datastream_private_vpc.name
+    access_config {}
+  }
+
+  service_account {
+    scopes = ["cloud-platform"]
+  }
+
+  metadata = {
+    gce-container-declaration = module.cloud_sql_auth_proxy_container_datastream.metadata_value
+    google-logging-enabled    = "true"
+    google-monitoring-enabled = "true"
+  }
+
+  labels = {
+    container-vm = module.cloud_sql_auth_proxy_container_datastream.vm_container_label
+  }
+}
+
+// Datastream connection profile for BigQuery target. Can be used by multiple streams.
+resource "google_datastream_connection_profile" "datastream_bigquery_connection_profile" {
+  display_name          = "datastream-bigquery-connection-profile"
+  location              = var.gcp_project["region"]
+  connection_profile_id = "datastream-bigquery-connection-profile"
+
+  bigquery_profile {}
+}
