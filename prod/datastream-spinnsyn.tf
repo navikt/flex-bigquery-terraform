a7211ae8f5cdc630b609dc89e49c4a2b644ebd3e
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2023-11-01 10:53:45 +0100

    Lag spinnsyn-datastream med 3600s data_freshness

--- prod/datastream-spinnsyn.tf
+++ prod/datastream-spinnsyn.tf
@@ -36,3 +36,52 @@ resource "google_datastream_connection_profile" "spinnsyn_postgresql_connection_
     private_connection = google_datastream_private_connection.flex_datastream_private_connection.id
   }
 }
+
+resource "google_datastream_stream" "spinnsyn_datastream" {
+  stream_id     = "spinnsyn-datastream"
+  display_name  = "spinnsyn-datastream"
+  desired_state = "RUNNING"
+  project       = var.gcp_project["project"]
+  location      = var.gcp_project["region"]
+  labels        = {}
+  backfill_all {}
+  timeouts {}
+
+  source_config {
+    source_connection_profile = google_datastream_connection_profile.spinnsyn_postgresql_connection_profile.id
+
+    postgresql_source_config {
+      max_concurrent_backfill_tasks = 0
+      publication                   = "spinnsyn_backend_publication"
+      replication_slot              = "spinnsyn_backend_replication"
+
+      exclude_objects {
+        postgresql_schemas {
+          schema = "public"
+
+          postgresql_tables {
+            table = "flyway_schema_history"
+          }
+        }
+      }
+
+      include_objects {
+        postgresql_schemas {
+          schema = "public"
+        }
+      }
+    }
+  }
+
+  destination_config {
+    destination_connection_profile = google_datastream_connection_profile.datastream_bigquery_connection_profile.id
+
+    bigquery_destination_config {
+      data_freshness = "3600s"
+
+      single_target_dataset {
+        dataset_id = "${var.gcp_project["project"]}:${google_bigquery_dataset.spinnsyn_datastream.dataset_id}"
+      }
+    }
+  }
+}
