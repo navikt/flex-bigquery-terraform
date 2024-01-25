b44f23b4e6e8c421eff657c234b6743d5b5f9ddc
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-01-25 15:50:13 +0100

    View for Soda avstemming av sendte sykepengesoknader

---
+++ prod/views-soda.tf
@@ -0,0 +1,27 @@
+module "soda_sendt_sykepengesoknad_avstemming" {
+  source              = "../modules/google-bigquery-view"
+  deletion_protection = false
+
+  dataset_id = google_bigquery_dataset.flex_dataset.dataset_id
+  view_id    = "soda-sendt-sykepengesoknad-avstemming"
+  view_schema = jsonencode(
+    [
+      {
+        name = "sykepengesoknad_uuid"
+        type = "STRING"
+      }
+    ]
+  )
+  view_query = <<EOF
+SELECT sykepengesoknad_uuid
+FROM `${var.gcp_project["project"]}.${google_bigquery_dataset.flex_dataset.dataset_id}.sykepengesoknad_sykepengesoknad_view`
+WHERE status = 'SENDT'
+  AND sendt < TIMESTAMP_SUB(current_timestamp(), INTERVAL 2 HOUR)
+  AND sendt >= "2024-01-01 00:00:00"
+  AND sykepengesoknad_uuid NOT IN (
+    SELECT sykepengesoknad_id FROM `${var.gcp_project["project"]}.${google_bigquery_dataset.flex_dataset.dataset_id}.sykepengesoknad_arkivering_oppgave_innsending_view`
+    WHERE behandlet >= "2024-01-01 00:00:00"
+  )
+EOF
+
+}
