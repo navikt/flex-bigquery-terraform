4bf260965d3682a0c023a1031884b3474eeeaf17
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-02-01 14:35:51 +0100

    Flytt navn på secrets til variables.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -11,41 +11,49 @@ variable "gcp_project" {
 variable "spinnsyn_bigquery_secret" {
   description = "The key of the GCP secret that provides the spinnsyn database credentials."
   type        = string
+  default     = "spinnsyn-bigquery-credentials"
 }
 
 variable "spinnsyn_datastream_secret" {
   description = "The key of the GCP secret that provides the spinnsyn datastream credentials."
   type        = string
+  default     = "spinnsyn-datastream-credentials"
 }
 
 variable "sykepengesoknad_bigquery_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad database credentials."
   type        = string
+  default     = "sykepengesoknad-bigquery-credentials"
 }
 
 variable "sykepengesoknad_datastream_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad datastream credentials."
   type        = string
+  default     = "sykepengesoknad-datastream-credentials"
 }
 
 variable "arkivering_oppgave_bigquery_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad-arkivering-oppgave database credentials."
   type        = string
+  default     = "arkivering-oppgave-bigquery-credentials"
 }
 
 variable "arkivering_oppgave_datastream_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad-arkivering-oppgave datastream credentials."
   type        = string
+  default     = "arkivering-oppgave-datastream-credentials"
 }
 
 variable "sak_status_metrikk_bigquery_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad-sak-status-metrikk database credentials."
   type        = string
+  default     = "sak-status-metrikk-bigquery-credentials"
 }
 
 variable "sak_status_metrikk_datastream_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad-sak-status-metrikk datastream credentials."
   type        = string
+  default     = "sak-status-metrikk-datastream-credentials"
 }
 
 variable "sykepengesoknad_cloud_sql_port" {
--- prod/variables.tf
+++ prod/variables.tf
@@ -1,7 +1,7 @@
 variable "gcp_project" {
   description = "GCP project and region defaults."
   type        = map(string)
-  default = {
+  default     = {
     region  = "europe-north1",
     zone    = "europe-north1-a",
     project = "flex-prod-af40"
@@ -23,51 +23,61 @@ variable "scheduled_query_data_source_id" {
 variable "spinnsyn_bigquery_secret" {
   description = "The key of the GCP secret that provides the spinnsyn database credentials."
   type        = string
+  default     = "spinnsyn-bigquery-credentials"
 }
 
 variable "spinnsyn_datastream_secret" {
   description = "The key of the GCP secret that provides the spinnsyn datastream credentials."
   type        = string
+  default     = "spinnsyn-datastream-credentials"
 }
 
 variable "sykepengesoknad_bigquery_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad database credentials."
   type        = string
+  default     = "sykepengesoknad-bigquery-credentials"
 }
 
 variable "sykepengesoknad_datastream_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad datastream credentials."
   type        = string
+  default     = "sykepengesoknad-datastream-credentials"
 }
 
 variable "arkivering_oppgave_bigquery_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad-arkivering-oppgave database credentials."
   type        = string
+  default     = "arkivering-oppgave-bigquery-credentials"
 }
 
 variable "arkivering_oppgave_datastream_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad-arkivering-oppgave datastream credentials."
   type        = string
+  default     = "arkivering-oppgave-datastream-credentials"
 }
 
 variable "sak_status_metrikk_bigquery_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad-sak-status-metrikk database credentials."
   type        = string
+  default     = "sak-status-metrikk-bigquery-credentials"
 }
 
 variable "sak_status_metrikk_datastream_secret" {
   description = "The key of the GCP secret that provides the sykepengesoknad-sak-status-metrikk datastream credentials."
   type        = string
+  default     = "sak-status-metrikk-datastream-credentials"
 }
 
 variable "flexjar_datastream_secret" {
   description = "The key of the GCP secret that provides the flexjar datastream credentials."
   type        = string
+  default     = "flexjar-datastream-credentials"
 }
 
 variable "modia_kontakt_metrikk_datastream_secret" {
   description = "The key of the GCP secret that provides the flex-modia-kontakt-metrikk datastream credentials."
   type        = string
+  default     = "flex-modia-kontakt-metrikk-datastream-credentials"
 }
 
 variable "sykepengesoknad_cloud_sql_port" {
