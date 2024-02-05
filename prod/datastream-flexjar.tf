e60663070bbb4f81c5a944bd62e8eb800aa4a5bc
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-02-05 14:22:44 +0100

    terraform format

--- prod/datastream-flexjar.tf
+++ prod/datastream-flexjar.tf
@@ -51,13 +51,13 @@ resource "google_bigquery_dataset" "flexjar_datastream" {
       table_id   = "flexjar_feedback_sykepengesoknad_kvittering_view"
     }
   }
-    access {
-      view {
-        dataset_id = "flex_dataset"
-        project_id = var.gcp_project["project"]
-        table_id   = "flexjar_feedback_view"
-      }
+  access {
+    view {
+      dataset_id = "flex_dataset"
+      project_id = var.gcp_project["project"]
+      table_id   = "flexjar_feedback_view"
     }
+  }
   access {
     view {
       dataset_id = "flex_dataset"
