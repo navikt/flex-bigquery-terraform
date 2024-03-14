eb0d6e8b12000d178b175fd5004ae9e229e3f2cc
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-03-14 12:45:13 +0100

    Opretter datastream for flex-inntektsmelding-status

--- prod/datastream-inntektsmelding-status.tf
+++ prod/datastream-inntektsmelding-status.tf
@@ -36,3 +36,53 @@ resource "google_datastream_connection_profile" "inntektsmelding_status_postgres
     private_connection = google_datastream_private_connection.flex_datastream_private_connection.id
   }
 }
+
+
+resource "google_datastream_stream" "inntektsmelding_status_datastream" {
+  stream_id     = "inntektsmelding-status-datastream"
+  display_name  = "inntektsmelding-status-datastream"
+  desired_state = "RUNNING"
+  project       = var.gcp_project["project"]
+  location      = var.gcp_project["region"]
+  labels        = {}
+  backfill_all {}
+  timeouts {}
+
+  source_config {
+    source_connection_profile = google_datastream_connection_profile.inntektsmelding_status_postgresql_connection_profile.id
+
+    postgresql_source_config {
+      max_concurrent_backfill_tasks = 0
+      publication                   = "flex_inntektsmelding_status_publication"
+      replication_slot              = "flex_inntektsmelding_status_replication"
+
+      exclude_objects {
+        postgresql_schemas {
+          schema = "public"
+
+          postgresql_tables {
+            table = "flyway_schema_history"
+          }
+        }
+      }
+
+      include_objects {
+        postgresql_schemas {
+          schema = "public"
+        }
+      }
+    }
+  }
+
+  destination_config {
+    destination_connection_profile = google_datastream_connection_profile.datastream_bigquery_connection_profile.id
+
+    bigquery_destination_config {
+      data_freshness = "3600s"
+
+      single_target_dataset {
+        dataset_id = "${var.gcp_project["project"]}:${google_bigquery_dataset.inntektsmelding_status_datastream.dataset_id}"
+      }
+    }
+  }
+}
