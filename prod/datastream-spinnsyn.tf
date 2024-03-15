6bb8ebee996d71600c1318f7cc36c3378e86d59a
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-03-15 09:16:19 +0100

    Access til spinnsyn view i flex dataset

--- prod/datastream-spinnsyn.tf
+++ prod/datastream-spinnsyn.tf
@@ -16,6 +16,13 @@ resource "google_bigquery_dataset" "spinnsyn_datastream" {
     role          = "WRITER"
     special_group = "projectWriters"
   }
+  access {
+    view {
+      dataset_id = "flex_dataset"
+      project_id = var.gcp_project["project"]
+      table_id   = "spinnsyn_utbetaling_view"
+    }
+  }
   timeouts {}
 }
 
