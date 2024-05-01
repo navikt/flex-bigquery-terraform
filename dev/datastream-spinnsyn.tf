a0ee824de067c6bdd1f979805d708e08045b500b
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-05-01 08:57:03 +0200

    Lag BigQuery dataset for Spinnsyn

---
+++ dev/datastream-spinnsyn.tf
@@ -0,0 +1,20 @@
+resource "google_bigquery_dataset" "spinnsyn_datastream" {
+  dataset_id = "spinnsyn_datastream"
+  location   = var.gcp_project["region"]
+  project    = var.gcp_project["project"]
+  labels     = {}
+
+  access {
+    role          = "OWNER"
+    special_group = "projectOwners"
+  }
+  access {
+    role          = "READER"
+    special_group = "projectReaders"
+  }
+  access {
+    role          = "WRITER"
+    special_group = "projectWriters"
+  }
+  timeouts {}
+}
