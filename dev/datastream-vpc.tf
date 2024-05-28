7a049becd41b32f33ca270f0a54238657bfa47bb
Nils Jørgen Mittet <88632815+nilsjorgen@users.noreply.github.com>
2024-05-28 10:19:09 +0200

    Lag modul for google_bigquery_datastream (#77)
    * Lag modul for å opprettet BigQuery Datastream
    
    * Formater kode
    
    * Bruk map for felles VPC-ressurser
    
    * Migrer spinnsyn-datastream til modul i DEV

--- dev/datastream-vpc.tf
+++ dev/datastream-vpc.tf
@@ -39,64 +39,10 @@ resource "google_compute_firewall" "allow_datastream_to_cloud_sql" {
 
   allow {
     protocol = "tcp"
-    ports = ["5432"]
+    ports    = ["5432"]
   }
 
   source_ranges = [google_datastream_private_connection.flex_datastream_private_connection.vpc_peering_config.0.subnet]
-}
-
-data "google_sql_database_instance" "spinnsyn_db" {
-  name = "spinnsyn-backend"
-}
-
-// You can configure a virtual machine (VM) instance or an instance template to deploy and launch a Docker container.
-// This Google-provided module handles the generation of metadata for deploying containers on GCE instances.
-module "cloud_sql_auth_proxy_container_datastream" {
-  source         = "terraform-google-modules/container-vm/google"
-  version        = "3.1.1"
-  cos_image_name = "cos-101-17162-386-65"
-  container = {
-    // https://github.com/GoogleCloudPlatform/cloud-sql-proxy/releases
-    image   = "eu.gcr.io/cloudsql-docker/gce-proxy:1.35.1"
-    command = ["/cloud_sql_proxy"]
-    args = [
-      "-instances=${data.google_sql_database_instance.spinnsyn_db.connection_name}=tcp:0.0.0.0:5432",
-    ]
-  }
-  restart_policy = "Always"
-}
-
-resource "google_compute_instance" "flex_datastream_cloud_sql_proxy_vm" {
-  name = "flex-datastream-cloud-sql-proxy-vm"
-  // Small machine type with 0.5 vCPU and 2 GB of memory, backed by a shared physical core.
-  machine_type              = "e2-small"
-  project                   = var.gcp_project["project"]
-  zone                      = var.gcp_project["zone"]
-  allow_stopping_for_update = true
-
-  boot_disk {
-    initialize_params {
-      image = module.cloud_sql_auth_proxy_container_datastream.source_image
-    }
-  }
-
-  network_interface {
-    network = google_compute_network.flex_datastream_private_vpc.name
-    // Ensures that a Private IP is assigned to the VM.
-    access_config {}
-  }
-
-  service_account {
-    scopes = ["cloud-platform"]
-  }
-
-  metadata = {
-    gce-container-declaration = module.cloud_sql_auth_proxy_container_datastream.metadata_value
-  }
-
-  labels = {
-    container-vm = module.cloud_sql_auth_proxy_container_datastream.vm_container_label
-  }
 }
 
 // Defines a Datastream connection profile for a BigQuery target.
---
+++ dev/datastreams.tf
@@ -0,0 +1,18 @@
+locals {
+  datastream_vpc_resources = {
+    vpc_name                       = google_compute_network.flex_datastream_private_vpc.name
+    private_connection_id          = google_datastream_private_connection.flex_datastream_private_connection.id
+    bigquery_connection_profile_id = google_datastream_connection_profile.datastream_bigquery_connection_profile.id
+  }
+}
+
+module "spinnsyn_datastream" {
+  source                            = "../modules/google-bigquery-datastream"
+  gcp_project                       = var.gcp_project
+  application_name                  = "spinnsyn"
+  cloud_sql_instance_name           = "spinnsyn-backend"
+  cloud_sql_instance_db_name        = "spinnsyn-db"
+  cloud_sql_instance_db_credentials = local.spinnsyn_datastream_credentials
+  datastream_vpc_resources          = local.datastream_vpc_resources
+}
+
