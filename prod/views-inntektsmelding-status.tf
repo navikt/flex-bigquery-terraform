8143214af05f6a2413affff88413e7585b80aa2d
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-06-27 14:03:01 +0200

    Revert "Slett gammelt view"
    This reverts commit f897bf11827dc872dac2e05e3e9850068ea0636f.

--- prod/views-inntektsmelding-status.tf
+++ prod/views-inntektsmelding-status.tf
@@ -1,3 +1,31 @@
+module "inntektsmelding_status_event_view" {
+  source = "../modules/google-bigquery-view"
+
+  dataset_id = google_bigquery_dataset.flex_dataset.dataset_id
+  view_id    = "inntektsmelding_status_event_view"
+  view_schema = jsonencode(
+    [
+      {
+        mode        = "NULLABLE"
+        name        = "opprettet"
+        type        = "TIMESTAMP"
+        description = "Tidspunkt for eventet."
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "status"
+        type        = "STRING"
+        description = "status på eventet"
+      }
+    ]
+  )
+  view_query = <<EOF
+SELECT opprettet, status
+FROM `${var.gcp_project["project"]}.${module.inntektsmelding_status_datastream.dataset_id}.public_inntektsmelding_status`
+EOF
+
+}
+
 module "venter_pa_arbeidsgiver_view" {
   source = "../modules/google-bigquery-view"
 
