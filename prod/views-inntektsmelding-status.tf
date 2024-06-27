18c9bf3ef885222c350ae3879cf6ae7e71abc192
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2024-06-27 13:58:45 +0200

    Varsling events view

--- prod/views-inntektsmelding-status.tf
+++ prod/views-inntektsmelding-status.tf
@@ -59,3 +59,46 @@ order by dager_siden_sendt desc
 EOF
 
 }
+
+
+module "varsling_events_view" {
+  source = "../modules/google-bigquery-view"
+
+  dataset_id = google_bigquery_dataset.flex_dataset.dataset_id
+  view_id    = "varsling_events_view"
+  view_schema = jsonencode(
+    [
+      {
+        mode        = "NULLABLE"
+        name        = "vedtaksperiode_id"
+        type        = "STRING"
+        description = ""
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "behandling_id"
+        type        = "STRING"
+        description = ""
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "status"
+        type        = "STRING"
+        description = ""
+      },
+      {
+        mode        = "NULLABLE"
+        name        = "tidspunkt"
+        type        = "TIMESTAMP"
+        description = ""
+      }
+    ]
+  )
+  view_query = <<EOF
+SELECT vb.vedtaksperiode_id, vb.behandling_id, vbs.status, vbs.tidspunkt 
+FROM `${var.gcp_project["project"]}.${module.inntektsmelding_status_datastream.dataset_id}.public_vedtaksperiode_behandling_status` vbs,
+`${var.gcp_project["project"]}.${module.inntektsmelding_status_datastream.dataset_id}.public_vedtaksperiode_behandling`  vb
+where vbs.vedtaksperiode_behandling_id = vb.id
+EOF
+
+}
