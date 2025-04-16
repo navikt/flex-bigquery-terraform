3073fe2c42ce10fd19966163dc554b8e0e6accc7
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2025-04-16 16:38:43 +0200

    some changes to prod

--- prod/datastreams.tf
+++ prod/datastreams.tf
@@ -220,3 +220,14 @@ module "flex_arbeidssokerregister_oppdatering_datastream" {
   datastream_vpc_resources                     = local.datastream_vpc_resources
   big_query_dataset_delete_contents_on_destroy = true
 }
+
+module "flex_sykmeldinger_backend_datastream" {
+  source                                       = "git::https://github.com/navikt/terraform-google-bigquery-datastream.git?ref=v1.0.4"
+  gcp_project                                  = var.gcp_project
+  application_name                             = "flex-sykmeldinger-backend"
+  cloud_sql_instance_name                      = "flex-sykmeldinger-backend"
+  cloud_sql_instance_db_name                   = "flex-sykmeldinger-backend-db"
+  cloud_sql_instance_db_credentials            = local.flex_sykmeldinger_datastream_backend_credentials
+  datastream_vpc_resources                     = local.datastream_vpc_resources
+  big_query_dataset_delete_contents_on_destroy = true
+}
