ff8765aa8b9aaad182bfc0500688354246d1eb66
oivindkulsrud <oivind.thorsen.kulsrud@nav.no>
2025-04-22 10:06:34 +0200

    Legger til datastream for flex-sykmeldinger (#111)

--- dev/secrets.tf
+++ dev/secrets.tf
@@ -30,3 +30,15 @@ locals {
     data.google_secret_manager_secret_version.flex_arbeidssokerregister_oppdatering_datastream_secret.secret_data
   )
 }
+
+
+data "google_secret_manager_secret_version" "flex_sykmeldinger_backend_datastream_credentials_secret" {
+  secret  = "flex-sykmeldinger-backend-datastream-credentials"
+  version = "1"
+}
+
+locals {
+  flex_sykmeldinger_datastream_backend_credentials = jsondecode(
+    data.google_secret_manager_secret_version.flex_sykmeldinger_backend_datastream_credentials_secret.secret_data
+  )
+}
