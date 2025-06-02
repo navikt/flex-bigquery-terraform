3fd58f689f71ee04b80b584d2014bbe9c7bc4b61
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2025-06-02 12:39:58 +0200

    external connection for sykmeldinger backend

--- prod/external-connection.tf
+++ prod/external-connection.tf
@@ -20,6 +20,17 @@ module "sykepengesoknad_bigquery_connection" {
   password      = local.sykepengesoknad_bigquery_credentials.password
 }
 
+module "sykmeldinger_bigquery_connection" {
+  source = "../modules/google-bigquery-connection"
+
+  connection_id = "flex-sykmeldinger-backend"
+  location      = var.gcp_project["region"]
+  instance_id   = "${var.gcp_project["project"]}:${var.gcp_project["region"]}:flex-sykmeldinger-backend"
+  database      = "sykepengesoknad"
+  username      = local.sykepengesoknad_bigquery_credentials.username
+  password      = local.sykepengesoknad_bigquery_credentials.password
+}
+
 module "sykepengesoknad_arkivering_oppgave_connection" {
   source = "../modules/google-bigquery-connection"
 
