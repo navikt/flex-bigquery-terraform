ece06c8bd04ed8ad30b0c8284b11b161d0a06d8d
Steinar Sørli Hegna <steinar.sorli.hegna@nav.no>
2026-01-09 14:26:56 +0100

    Legge til sykmelding siste hendelse brukersvar view

--- prod/views-sykmeldinger.tf
+++ prod/views-sykmeldinger.tf
@@ -123,3 +123,88 @@ module "sykmeldinger_med_hendelser_view" {
         sm.fnr
   EOF
 }
+
+module "sykmeldinger_siste_hendelse_brukersvar_view" {
+  source = "../modules/google-bigquery-view"
+
+  deletion_protection = false
+  dataset_id          = google_bigquery_dataset.flex_dataset.dataset_id
+  view_id             = "sykmeldinger_siste_hendelse_brukersvar_view"
+  view_schema = jsonencode(
+    [
+      {
+        name = "sykmelding_id",
+        mode = "NULLABLE",
+        type = "STRING"
+      },
+      {
+        name = "sykmelding_opprettet",
+        mode = "NULLABLE",
+        type = "TIMESTAMP"
+      },
+      {
+        name = "hendelse_opprettet",
+        mode = "NULLABLE",
+        type = "TIMESTAMP"
+      },
+      {
+        name = "status",
+        mode = "NULLABLE",
+        type = "STRING"
+      },
+      {
+        name = "arbeidssituasjon",
+        mode = "NULLABLE",
+        type = "STRING"
+      },
+      {
+        name = "erOpplysningeneRiktige",
+        mode = "NULLABLE",
+        type = "BOOL"
+      },
+      {
+        name = "riktigNarmesteLeder",
+        mode = "NULLABLE",
+        type = "BOOL"
+      },
+      {
+        name = "harEgenmeldingsdager",
+        mode = "NULLABLE",
+        type = "BOOL"
+      },
+      {
+        name = "harBruktEgenmelding",
+        mode = "NULLABLE",
+        type = "BOOL"
+      },
+      {
+        name = "harForsikring",
+        mode = "NULLABLE",
+        type = "BOOL"
+      }
+    ]
+  )
+
+  view_query = <<EOF
+      SELECT sm.sykmelding_id                                                                 as sykmelding_id,
+             sm.opprettet                                                                     AS sykmelding_opprettet,
+             siste_smh.hendelse_opprettet                                                     AS hendelse_opprettet,
+             siste_smh.status                                                                 as status,
+             JSON_VALUE(siste_smh.bruker_svar, '$.arbeidssituasjon.svar')                     AS arbeidssituasjon,
+             CAST(JSON_VALUE(siste_smh.bruker_svar, '$.erOpplysningeneRiktige.svar') AS BOOL) AS erOpplysningeneRiktige,
+             CAST(JSON_VALUE(siste_smh.bruker_svar, '$.riktigNarmesteLeder.svar') AS BOOL)    AS riktigNarmesteLeder,
+             CAST(JSON_VALUE(siste_smh.bruker_svar, '$.harEgenmeldingsdager.svar') AS BOOL)   AS harEgenmeldingsdager,
+             CAST(JSON_VALUE(siste_smh.bruker_svar, '$.harBruktEgenmelding.svar') AS BOOL)    AS harBruktEgenmelding,
+             CAST(JSON_VALUE(siste_smh.bruker_svar, '$.harForsikring.svar') AS BOOL)          AS harForsikring
+
+      FROM `${var.gcp_project["project"]}.${module.flex_sykmeldinger_backend_datastream.dataset_id}.public_sykmelding` sm
+               JOIN (SELECT *
+                     FROM `${var.gcp_project["project"]}.${module.flex_sykmeldinger_backend_datastream.dataset_id}.public_sykmeldinghendelse`
+
+                     QUALIFY ROW_NUMBER() OVER (
+                         PARTITION BY sykmelding_id
+                         ORDER BY hendelse_opprettet DESC
+                         ) = 1) siste_smh ON sm.sykmelding_id = siste_smh.sykmelding_id
+      where siste_smh.bruker_svar is not null
+    EOF
+}
